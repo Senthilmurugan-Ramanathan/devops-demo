8ada1e507e3c7b045793466ee213f747cf20a23e
Ramanathan <598615@cognizant.com>
2017-05-30 09:38:35 +0530

    intial

--- angularjs-springboot-bookstore-master/src/main/webapp/scripts/components/auth/services/register.service.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('bookstoreApp')
-    .factory('Register', function ($resource) {
-        return $resource('api/register', {}, {
-        });
-    });
-
-
--- angularjs-springboot-bookstore-master/src/main/webapp/scripts/components/form/pagination.directive.js
+++
@@ -1,9 +0,0 @@
-/* globals $ */
-'use strict';
-
-angular.module('bookstoreApp')
-    .directive('bookstoreAppPagination', function() {
-        return {
-            templateUrl: 'scripts/components/form/pagination.html'
-        };
-    });
